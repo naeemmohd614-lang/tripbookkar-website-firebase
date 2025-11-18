cfe6b38677afa5f67c34626f219233960558e110
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-11-18 09:05:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1865376664-5e932",
+  "appId": "1:310548221344:web:e7d892259b75ccc36b68c5",
+  "apiKey": "AIzaSyDhh7F5TapGkd68FAkSiZBXSrus7f6dVHg",
+  "authDomain": "studio-1865376664-5e932.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "310548221344"
+};
