68c2ce4df8ad93c7e33c4517dc109e842825ea63
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-11-25 01:44:06 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         hostname: 'media.istockphoto.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.hiltonstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
