28df971008271f2a3305368600deedd48ca6ed33
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-11-25 18:17:10 +0000

    esko TripBookkar ki jagah logo laga do

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { Mountain } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 font-bold text-lg text-brand-blue", className)}>
-        <Mountain className="h-6 w-6" />
-        <span className="font-headline text-xl">TripBookKar</span>
+        <Image src="/TripBookkar.png" alt="TripBookkar Logo" width={150} height={50} />
     </Link>
   );
 }
