8947ce7e14e8a94833358d49e6716f7419ac472f
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-11-30 14:20:58 +0000

    use this link https://www.vhv.rs/dpng/d/474-4740043_taj-hotels-logo-png-

--- next.config.ts
+++ next.config.ts
@@ -101,6 +101,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.vhv.rs',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
