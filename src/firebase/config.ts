534261577ab8ac3a219ffb63183507808e6a3555
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-12-03 13:17:31 +0000

    "site": "tripiff-1cdde",

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDstrka_cRhxTSEvy_ujqdG0HtnHdyQu-M",
-  authDomain: "tripiff.firebaseapp.com",
-  projectId: "tripiff",
-  storageBucket: "tripiff.firebasestorage.app",
+  authDomain: "tripiff-1cdde.firebaseapp.com",
+  projectId: "tripiff-1cdde",
+  storageBucket: "tripiff-1cdde.appspot.com",
   messagingSenderId: "571956288642",
   appId: "1:571956288642:web:6887d1119665cb2c7de89b",
   measurementId: "G-HCDRQ0EBGS"
