6e0ea26796d0e9859cd5f8f7394fb4abaca9c363
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-12-05 00:26:08 +0000

    ❯ studio-1865376664-5e932 (Firebase app) yah wala project add karna hao

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDstrka_cRhxTSEvy_ujqdG0HtnHdyQu-M",
-  authDomain: "tripiff-1cdde.firebaseapp.com",
-  projectId: "tripiff-1cdde",
-  storageBucket: "tripiff-1cdde.appspot.com",
+  authDomain: "studio-1865376664-5e932.firebaseapp.com",
+  projectId: "studio-1865376664-5e932",
+  storageBucket: "studio-1865376664-5e932.appspot.com",
   messagingSenderId: "571956288642",
   appId: "1:571956288642:web:6887d1119665cb2c7de89b",
   measurementId: "G-HCDRQ0EBGS"
